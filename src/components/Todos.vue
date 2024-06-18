3472dc32f139002d8d32eb2a423c65f37858c212
Rudolf Bárdos <bardos@outlook.com>
2024-06-18 13:18:29 +0200

    feat: add auth

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -7,27 +7,31 @@ import { generateClient } from 'aws-amplify/data';
 const client = generateClient<Schema>();
 
 // create a reactive reference to the array of todos
-const todos = ref<Array<Schema['Todo']["type"]>>([]);
+const todos = ref<Array<Schema['Todo']['type']>>([]);
 
 function listTodos() {
   client.models.Todo.observeQuery().subscribe({
     next: ({ items, isSynced }) => {
-      todos.value = items
-     },
-  }); 
+      todos.value = items;
+    }
+  });
+}
+
+function deleteTodo(id: string) {
+  client.models.Todo.delete({ id });
 }
 
 function createTodo() {
   client.models.Todo.create({
-    content: window.prompt("Todo content")
+    content: window.prompt('Todo content')
   }).then(() => {
     // After creating a new todo, update the list of todos
     listTodos();
   });
 }
-    
+
 // fetch todos when the component is mounted
- onMounted(() => {
+onMounted(() => {
   listTodos();
 });
 
@@ -38,15 +42,16 @@ function createTodo() {
     <h1>My todos</h1>
     <button @click="createTodo">+ new</button>
     <ul>
-      <li 
-        v-for="todo in todos" 
-        :key="todo.id">
+      <li
+          v-for="todo in todos"
+          :key="todo.id"
+          @click="deleteTodo(todo.id)">
         {{ todo.content }}
       </li>
     </ul>
     <div>
       🥳 App successfully hosted. Try creating a new todo.
-      <br />
+      <br/>
       <a href="https://docs.amplify.aws/gen2/start/quickstart/nextjs-pages-router/">
         Review next steps of this tutorial.
       </a>
